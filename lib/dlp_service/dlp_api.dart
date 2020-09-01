1538c765beef63e8ca2e5b5666ab424119aa193e
leenorshn <katembovictor@gmail.com>
2020-09-01 16:47:09 +0100

    loggin data

--- lib/dlp_service/dlp_api.dart
+++ lib/dlp_service/dlp_api.dart
@@ -48,6 +48,7 @@ class DlpApi {
     var response = await http.get(_baseUrl + "/api/accounts/$phone",
         headers: {"content-type": "application/json"});
     if (response.statusCode == 200) {
+      print(response.body);
       return AccountInfo.fromJson(response.body);
     } else {
       return null;
