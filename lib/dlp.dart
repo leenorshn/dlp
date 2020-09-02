769b9e5b77c9279d25ae302d976eec8cd924cf8a
leenorshn <katembovictor@gmail.com>
2020-09-02 10:48:35 +0100

    using dialog in place off a simple card widget

--- lib/dlp.dart
+++ lib/dlp.dart
@@ -1,5 +1,18 @@
 library dlp;
 
+import 'package:dlp/src/dlp_payment_widget.dart';
+import 'package:flutter/material.dart';
+
 export 'src/account_info.dart';
-export 'src/dlp_payment_widget.dart';
 export 'src/dlp_service.dart';
+
+class DlpWidget {
+  show(BuildContext context, {phone, amount, pin, provider}) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return DlpPaymentWidget(
+              phone: phone, amount: amount, pin: pin, provider: provider);
+        });
+  }
+}
