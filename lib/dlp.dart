4ad9e726949b98e3a0a9d6b5a0e8eb1e90f6abb2
leenorshn <katembovictor@gmail.com>
2020-09-05 14:48:13 +0100

    exporting librairie

--- lib/dlp.dart
+++ lib/dlp.dart
@@ -3,6 +3,7 @@ library dlp;
 import 'package:dlp/src/dlp_payment_widget.dart';
 import 'package:flutter/material.dart';
 
+export 'src/create_account_widget.dart';
 export 'src/dlp_service.dart';
 export 'src/models/account_info.dart';
 
